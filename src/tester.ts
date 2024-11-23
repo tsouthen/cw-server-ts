a572590a84e78d3e585044051e592eff0d9f6783
Todd Southen <tsouthen@gmail.com>
2024-11-23 11:00:32 -0800

    Support argv with tester

--- src/tester.ts
+++ src/tester.ts
@@ -1,7 +1,9 @@
 import { getWeatherData } from "./getWeatherData";
 
 async function tester() {
-  const data = await getWeatherData("courtenay");
+  const request = process.argv.length > 2 ? process.argv[process.argv.length - 1] : "courtenay";
+  const data = await getWeatherData(request);
   console.log(data);
+  process.exit(0);
 }
 tester();
